0d0c579d0a88bf98d642c73416a7a62c26a89886
BeBaal <86570412+BeBaal@users.noreply.github.com>
2022-10-03 13:39:57 +0200

    Update

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
+# Import Statements
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Declaration of class variables
+
 
 def main():
+    # declaration of method variables
+    monthlyContribution = 200   # how much money you put in each month
+    months = 12                 # how many months you invest for
+    previousAccountBalance = 0  # help variable / ToDo exchange for direct reference
 
-    # Load Excel files
+    # Load Excel file
     xls = pd.ExcelFile(
         '../Daten/Daten_Studienarbeit_Optimierung_Kapitalertragssteuer.xlsx')
-    df_MSCI_World = pd.read_excel(
-        xls,  'MSCI_World', header=0)
-    df_MSCI_EM = pd.read_excel(xls, 'MSCI_EM')
-    df_MSCI_ACWI = pd.read_excel(xls, 'MSCI_ACWI')
 
-    # Load Dataframes
-    d_MSCI_World = pd.DataFrame(
-        df_MSCI_World, columns=['Date', 'Value in USD'])
-    d_MSCI_EM = pd.DataFrame(
-        df_MSCI_EM, columns=['Date', 'Value in USD'])
-    d_MSCI_ACWI = pd.DataFrame(
-        df_MSCI_ACWI, columns=['Date', 'Value in USD'])
-    # print(d_MSCI_World, d_MSCI_EM, d_MSCI_ACWI)    # Debugging Code
+    # Load Excel sheets
+    df = [pd.read_excel(xls,  'MSCI_World', header=0),
+          pd.read_excel(xls, 'MSCI_EM', header=0),
+          pd.read_excel(xls, 'MSCI_ACWI', header=0)]
 
-    # Change Type to numeric and strings are parsed to NaN, replace by 0
-    # d_MSCI_World['Value in USD'] = pd.to_numeric(
-    #     d_MSCI_World['Value in USD'], errors='coerce').fillna(0).astype(complex)
-    # d_MSCI_EM['Value in USD'] = pd.to_numeric(d_MSCI_EM['Value in USD'])
-    # d_MSCI_ACWI['Value in USD'] = pd.to_numeric(
-    #    d_MSCI_ACWI['Value in USD'], errors='coerce').fillna(0).astype(complex)
+    # Create array of dataframes for looping
+    data = [pd.DataFrame(df[0], columns=['Date', 'Value in USD']),
+            pd.DataFrame(df[1], columns=['Date', 'Value in USD']),
+            pd.DataFrame(df[2], columns=['Date', 'Value in USD'])
+            ]
 
-    # Add Column
-    d_MSCI_World['%-change (base 100)'] = d_MSCI_World['Value in USD'] / \
-        d_MSCI_World['Value in USD'][0]
-    d_MSCI_EM['%-change (base 100)'] = d_MSCI_EM['Value in USD'] / \
-        d_MSCI_EM['Value in USD'][0]
-    d_MSCI_ACWI['%-change (base 100)'] = d_MSCI_ACWI['Value in USD'] / \
-        d_MSCI_ACWI['Value in USD'][0]
+    # print(data[0], data[1], data[2])    # Debugging Code
 
-    print(d_MSCI_World, d_MSCI_EM, d_MSCI_ACWI)    # Debugging Code
+    # Add Columns and do simple calculation
+    for dataframe in data:
+        dataframe['%-change (base 100)'] = dataframe['Value in USD'] / \
+            dataframe['Value in USD'][0]  # set relative values from first value
+        dataframe['monthlyContribution'] = monthlyContribution
+        dataframe['monthlyContribution accumulation'] = 0
+        dataframe['Account Balance'] = 0
+
+        # print(dataframe)  # Debugging Code
+
+    # iterate over each row in dataframe and do the calculation
+    for row in data[0].iterrows():
+
+        if months - row[0] >= 1:
+            row[1][4] = (row[0] + 1) * monthlyContribution
+            # row[1][2] # i do not think this is the right logic
+            row[1][5] = row[1][3] + previousAccountBalance * 1.10  # placeholder
+
+            # Row to table Reference
+            dataframe['monthlyContribution accumulation'][row[0]
+                                                          ] = row[1][4]  # monthlyCont accu
+            dataframe['Account Balance'][row[0]] = row[1][5]  # Account Balance
+
+            print(row[0])     # Table index
+            print(row[1][2])  # %-change (base 100)
+            print(row[1][3])  # monthlyContribution
+            print(row[1][4])  # monthlyContribution accumulation
+            print(row[1][5])  # Account Balance
+            print(row)
+            print()
+
+            # Remember former Account Balance
+            previousAccountBalance = row[1][5]
+
+    # Sell stocks each year and add sold value to variable
+
+    # Export results for each dataframe to CSV file
+    for counter, dataframe in enumerate(data):
+        dataframe.to_csv('Dataframe_Export_' + str(counter) + '.CSV')
+
+    # Iterate with different values
+
+    # Plot results
 
 
 # main function call
--- test_main.py
+++ test_main.py
@@ -1,9 +1,52 @@
 import pytest
+import pandas as pd
+import datatest as dt
 from main import main
 main()
 
+# load the excel files
+# ToDo simplify code and return array with tables / dataframes to loop over
 
-def test_import_sheet_MSCI_World():
 
-    print("")
-    assert bool("")
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df_1():
+    xls = pd.ExcelFile(
+        '../Daten/Daten_Studienarbeit_Optimierung_Kapitalertragssteuer.xlsx')
+    return pd.read_excel(xls, 'MSCI_World', header=0)
+
+
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df_2():
+    xls = pd.ExcelFile(
+        '../Daten/Daten_Studienarbeit_Optimierung_Kapitalertragssteuer.xlsx')
+    return pd.read_excel(xls, 'MSCI_EM', header=0)
+
+
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df_3():
+    xls = pd.ExcelFile(
+        '../Daten/Daten_Studienarbeit_Optimierung_Kapitalertragssteuer.xlsx')
+    return pd.read_excel(xls, 'MSCI_ACWI', header=0)
+
+
+# Do the title columns have the right names
+def test_columns(df_1, df_2, df_3):
+    dt.validate(df_1.columns[0], {'Date'})
+    dt.validate(df_1.columns[1], {'Value in USD'})
+    dt.validate(df_2.columns[0], {'Date'})
+    dt.validate(df_2.columns[1], {'Value in USD'})
+    dt.validate(df_3.columns[0], {'Date'})
+    dt.validate(df_3.columns[1], {'Value in USD'})
+
+
+# Are the values numbers
+
+
+@pytest.mark.mandatory
+def test_values(df_1, df_2, df_3):
+    dt.validate(df_1['Value in USD'], int)
+    dt.validate(df_2['Value in USD'], int)
+    dt.validate(df_3['Value in USD'], int)
